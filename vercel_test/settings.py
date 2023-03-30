b8ce7f637c3345c1e5027922bb738d8efbeb6bc2
Sri-1227 <srinivasmekala1227@gmail.com>
2023-03-30 07:56:15 +0530

    production setup

--- vercel_test/settings.py
+++ vercel_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!9e&n+8&+18rm*)1+7=1tal#%w@(tgh3rx+(#*k_gzhdnuq!w&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
